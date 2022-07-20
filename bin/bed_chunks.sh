e46643ca6e78310d6ce04e7560ad2ecf58a0cbc2
Ksenia Krasheninnikova <kk16@sanger.ac.uk>
2022-07-20 10:35:40 +0100

    Polishing Workflow: refactor code in bash scripts, remove redundant setting in config

--- bin/bed_chunks.sh
+++ bin/bed_chunks.sh
@@ -3,10 +3,9 @@
 export FAI=$1
 export CHUNKS=$2
 
-for entry in $(cat ${FAI} | awk '{print NR":"$1":"$2}' ); do
-    chunk_id=$(echo $entry | cut -f 1 -d':');
+for entry in $(awk '{print NR":"$1":"$2}' ${FAI}); do
+    chunk_id=$(echo $entry | cut -f 1 -d':' );
     chunk_id=$(( $chunk_id % ${CHUNKS} ));
-    echo ${GROUPS} $chunk_id
     contig=$(echo $entry | cut -f 2 -d':');
     end=$(echo $entry | cut -f 3 -d':');
     echo -e ${contig}"\t0\t"${end} >> ${chunk_id}.bed
--- bin/coverage.sh
+++ bin/coverage.sh
@@ -3,6 +3,5 @@
 export SUMMARY=$1
 
 line=$(less ${SUMMARY} | head -n 1 | sed 's/,/\n/g' | awk '{print NR"\t"$1}' | grep mean_depth | cut -f1)
-cov=$(less ${SUMMARY} | tail -n 1 | sed 's/,/\n/g' | head -n ${line} | tail -n1)
-cov=$(echo ${cov} | sed 's/\..*//g')
+cov=$(less ${SUMMARY} | tail -n 1 | sed 's/,/\n/g' | head -n ${line} | tail -n1 | sed 's/\..*//g')
 awk -v cov="$cov" 'BEGIN {print cov*12}'
