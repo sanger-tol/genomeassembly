5f8a6aca3ba0cfa6f319b1e7da27c7df4950d03f
Ksenia Krasheninnikova <kk16@sanger.ac.uk>
2022-07-20 16:24:37 +0100

    Polising Workflow: bash code optimisation

--- bin/bed_chunks.sh
+++ bin/bed_chunks.sh
@@ -4,8 +4,7 @@ export FAI=$1
 export CHUNKS=$2
 
 for entry in $(awk '{print NR":"$1":"$2}' ${FAI}); do
-    chunk_id=$(echo $entry | cut -f 1 -d':' );
-    chunk_id=$(( $chunk_id % ${CHUNKS} ));
+    chunk_id=$(( $(echo $entry | cut -f 1 -d':') % ${CHUNKS}  ));
     contig=$(echo $entry | cut -f 2 -d':');
     end=$(echo $entry | cut -f 3 -d':');
     echo -e ${contig}"\t0\t"${end} >> ${chunk_id}.bed
