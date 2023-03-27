98579a455369530f94a4543e44959d9d5fa15f66
Ksenia Krasheninnikova <kk16@sanger.ac.uk>
2023-03-27 15:14:34 +0100

    Formatting changes

--- bin/get_calcuts_params_from_model_fk.py
+++ bin/get_calcuts_params_from_model_fk.py
@@ -2,19 +2,20 @@
 
 import sys
 
+
 def extract_cov(path):
     with open(path) as f:
         for line in f:
             line = line.strip().split()
-            if line and line[0] == 'kmercov':
+            if line and line[0] == "kmercov":
                 return float(line[1])
     return -1
-            
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     path = sys.argv[1]
     cov = extract_cov(path)
-    cov = int(cov + cov/2)
-    max_val = cov*4
-    calcuts_opts = '-m {:d} -u {:d}'.format(cov, max_val)
+    cov = int(cov + cov / 2)
+    max_val = cov * 4
+    calcuts_opts = "-m {:d} -u {:d}".format(cov, max_val)
     print(calcuts_opts)
