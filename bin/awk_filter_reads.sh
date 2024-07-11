88eeafbc2f601817c5150a5a89c6ef1be8e0c1fe
yumisims <yy5@sanger.ac.uk>
2024-07-11 20:20:38 +0100

    bug in awk_filter_reads

--- bin/awk_filter_reads.sh
+++ bin/awk_filter_reads.sh
@@ -1,1 +1,2 @@
-awk 'BEGIN{OFS="\t"}{if($1 ~ /^@/) {print} else {$2=and($2,compl(2048)); print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11}}'
+#remove the read number added earlier --yy5
+awk 'BEGIN{OFS="\t"}{if($1 ~ /^@/) {print} else {$2=and($2,compl(2048)); print substr($1,2),$2,$3,$4,$5,$6,$7,$8,$9,$10,$11}}'
